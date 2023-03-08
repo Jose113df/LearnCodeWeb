3b3df35ca9e77951a4051f285e02bff0c5fb4c2f
jose113df <josemateusti@gmail.com>
2023-03-08 11:18:05 -0300

    Codigo WEB

---
+++ source/repos/LearnCode.Web/LearnCode.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LearnCode.Web.MvcApplication" Language="C#" %>
---
+++ source/repos/LearnCode.Web/LearnCode.Web/Startup1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(LearnCode.Web.Startup1))]
+
+namespace LearnCode.Web
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType= "AplicationCookie",
+                //Rota (endereço URL)
+                 LoginPath = new PathString("/Autenticacao/Login")
+            });
+
+            // Para obter mais informações sobre como configurar seu aplicativo, visite https://go.microsoft.com/fwlink/?LinkID=316888
+        //colocar a codificação que se encontra no roteiro de copetencia(.docx)
+        }
+    }
+}
